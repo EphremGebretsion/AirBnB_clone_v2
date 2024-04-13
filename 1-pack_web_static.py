a5ec6b42f3c7b983d5cf77fdbffb16fd76fdcfe4
Ephrem Gebretsion <ephhrem@gmail.com>
2024-04-14 02:47:28 +0300

    packing webstatic to archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python3
+"""
+packs web_static into to versions folder
+"""
 from fabric.api import local
+
+
 def do_pack():
+    """paking webstatic to webst_atic_<date>.tgz archive"""
     date = local('date +"%Y%m%d%H%M%S"', capture=True)
     store = f"versions/web_static_{date}.tgz"
     local("if [! -d versions ];then mkdir versions; fi")
