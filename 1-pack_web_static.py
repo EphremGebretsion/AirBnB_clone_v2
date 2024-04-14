88baf744dfd302f397d2bbe5976d6d44ff8dcf93
Ephrem Gebretsion <ephhrem@gmail.com>
2024-04-14 03:37:29 +0300

    pack web_static fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,6 +9,6 @@ def do_pack():
     """paking webstatic to webst_atic_<date>.tgz archive"""
     date = local('date +"%Y%m%d%H%M%S"', capture=True)
     store = f"versions/web_static_{date}.tgz"
-    local("if [! -d versions ];then mkdir versions; fi")
+    local("if [ ! -d versions ];then mkdir versions; fi")
     local(f"echo Packing web_static to {store}")
     local(f"tar -cvzf {store} web_static")
